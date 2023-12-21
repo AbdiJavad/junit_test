802ed7c32d4d0b3cb24ae56844ccc27aa1e056db
AbdiJavad <Mirjavadabdi@gmail.com>
2023-12-21 23:30:50 +0330

    first commit

---
+++ src/ArrayListTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+public class ArrayListTest {
+    private ArrayList<String> list =new ArrayList<String>();
+
+    @Before
+    public void setList() throws Exception{
+
+    }
+
+    @Test
+    public void testDeletion(){
+        list.clear();
+        assertTrue(list.isEmpty());
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.remove("B");
+        assertEquals(2,list.size());
+
+    }
+}
+
---
+++ src/Atest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class Atest {
+
+    @Test
+    public void m1() {
+
+    }
+
+    @Test
+    public void m2() {
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
