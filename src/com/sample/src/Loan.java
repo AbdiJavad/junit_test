efc59a0ed5f47c4535e912d24ba1710913d72137
AbdiJavad <Mirjavadabdi@gmail.com>
2023-12-23 00:59:09 +0330

    Updated,Changed

--- src/com/sample/src/Loan.java
+++ src/com/sample/src/Loan.java
@@ -11,7 +11,6 @@ public class Loan {
     public Loan() {
         this(2.5, 1, 1000);
     }
-
     public Loan(double annualInterestRate, int numberOfYears, double loanAmount) {
         this.annualInterestRate = annualInterestRate;
         this.numberOfYears = numberOfYears;
--- test/com/sample/test/LoanTest.java
+++ test/com/sample/test/LoanTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static junit.framework.Assert.assertTrue;
 
 public class LoanTest {
-    
+
     @Before
     public void setUp() throws Exception {
     }
