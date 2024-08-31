359c1dd754fc18fe8f9cb160a7ce9b49795e8651
JavadAbdi <Mirjavadabdi@gmail.com>
2024-08-31 17:08:26 +0330

    Updated,Changed

--- src/com/sample/src/Loan.java
+++ src/com/sample/src/Loan.java
@@ -53,8 +53,7 @@ public class Loan {
                 (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
         return monthlyPayment;
     }
-
-    public double getTotalPayment() {
+        public double getTotalPayment() {
         double totalPayment = getMonthlyPayment() * numberOfYears * 12;
         return totalPayment;
     }
